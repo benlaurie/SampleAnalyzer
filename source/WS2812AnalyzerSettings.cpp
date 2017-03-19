345cc52c20c70cd678be943f2547a23b54875194
Ben Laurie <benl@google.com>
2017-03-19 17:27:07 +0000

    Reformat.

--- source/WS2812AnalyzerSettings.cpp
+++ source/WS2812AnalyzerSettings.cpp
@@ -3,71 +3,65 @@
 
 
 WS2812AnalyzerSettings::WS2812AnalyzerSettings()
-:	mInputChannel( UNDEFINED_CHANNEL ),
-	mBitRate( 9600 )
-{
-	mInputChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mInputChannelInterface->SetTitleAndTooltip( "Serial", "Standard WS2812" );
-	mInputChannelInterface->SetChannel( mInputChannel );
+  : mInputChannel(UNDEFINED_CHANNEL),
+    mBitRate(9600) {
+  mInputChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+  mInputChannelInterface->SetTitleAndTooltip( "Serial", "Standard WS2812" );
+  mInputChannelInterface->SetChannel( mInputChannel );
 
-	mBitRateInterface.reset( new AnalyzerSettingInterfaceInteger() );
-	mBitRateInterface->SetTitleAndTooltip( "Bit Rate (Bits/S)",  "Specify the bit rate in bits per second." );
-	mBitRateInterface->SetMax( 6000000 );
-	mBitRateInterface->SetMin( 1 );
-	mBitRateInterface->SetInteger( mBitRate );
+  mBitRateInterface.reset( new AnalyzerSettingInterfaceInteger() );
+  mBitRateInterface->SetTitleAndTooltip( "Bit Rate (Bits/S)",  "Specify the bit rate in bits per second." );
+  mBitRateInterface->SetMax( 6000000 );
+  mBitRateInterface->SetMin( 1 );
+  mBitRateInterface->SetInteger( mBitRate );
 
-	AddInterface( mInputChannelInterface.get() );
-	AddInterface( mBitRateInterface.get() );
+  AddInterface( mInputChannelInterface.get() );
+  AddInterface( mBitRateInterface.get() );
 
-	AddExportOption( 0, "Export as text/csv file" );
-	AddExportExtension( 0, "text", "txt" );
-	AddExportExtension( 0, "csv", "csv" );
+  AddExportOption( 0, "Export as text/csv file" );
+  AddExportExtension( 0, "text", "txt" );
+  AddExportExtension( 0, "csv", "csv" );
 
-	ClearChannels();
-	AddChannel( mInputChannel, "Serial", false );
+  ClearChannels();
+  AddChannel( mInputChannel, "Serial", false );
 }
 
-WS2812AnalyzerSettings::~WS2812AnalyzerSettings()
-{
+WS2812AnalyzerSettings::~WS2812AnalyzerSettings() {
 }
 
-bool WS2812AnalyzerSettings::SetSettingsFromInterfaces()
-{
-	mInputChannel = mInputChannelInterface->GetChannel();
-	mBitRate = mBitRateInterface->GetInteger();
+bool WS2812AnalyzerSettings::SetSettingsFromInterfaces() {
+  mInputChannel = mInputChannelInterface->GetChannel();
+  mBitRate = mBitRateInterface->GetInteger();
 
-	ClearChannels();
-	AddChannel( mInputChannel, "WS2812", true );
+  ClearChannels();
+  AddChannel( mInputChannel, "WS2812", true );
 
-	return true;
+  return true;
 }
 
-void WS2812AnalyzerSettings::UpdateInterfacesFromSettings()
-{
-	mInputChannelInterface->SetChannel( mInputChannel );
-	mBitRateInterface->SetInteger( mBitRate );
+void WS2812AnalyzerSettings::UpdateInterfacesFromSettings() {
+  mInputChannelInterface->SetChannel( mInputChannel );
+  mBitRateInterface->SetInteger( mBitRate );
 }
 
-void WS2812AnalyzerSettings::LoadSettings( const char* settings )
-{
-	SimpleArchive text_archive;
-	text_archive.SetString( settings );
+void WS2812AnalyzerSettings::LoadSettings( const char* settings ) {
+  SimpleArchive text_archive;
+  text_archive.SetString( settings );
 
-	text_archive >> mInputChannel;
-	text_archive >> mBitRate;
+  text_archive >> mInputChannel;
+  text_archive >> mBitRate;
 
-	ClearChannels();
-	AddChannel( mInputChannel, "WS2812", true );
+  ClearChannels();
+  AddChannel( mInputChannel, "WS2812", true );
 
-	UpdateInterfacesFromSettings();
+  UpdateInterfacesFromSettings();
 }
 
-const char* WS2812AnalyzerSettings::SaveSettings()
-{
-	SimpleArchive text_archive;
+const char* WS2812AnalyzerSettings::SaveSettings() {
+  SimpleArchive text_archive;
 
-	text_archive << mInputChannel;
-	text_archive << mBitRate;
+  text_archive << mInputChannel;
+  text_archive << mBitRate;
 
-	return SetReturnString( text_archive.GetString() );
+  return SetReturnString( text_archive.GetString() );
 }
--- source/WS2812AnalyzerSettings.h
+++ source/WS2812AnalyzerSettings.h
@@ -4,24 +4,23 @@
 #include <AnalyzerSettings.h>
 #include <AnalyzerTypes.h>
 
-class WS2812AnalyzerSettings : public AnalyzerSettings
-{
+class WS2812AnalyzerSettings : public AnalyzerSettings {
 public:
-	WS2812AnalyzerSettings();
-	virtual ~WS2812AnalyzerSettings();
+  WS2812AnalyzerSettings();
+  virtual ~WS2812AnalyzerSettings();
 
-	virtual bool SetSettingsFromInterfaces();
-	void UpdateInterfacesFromSettings();
-	virtual void LoadSettings( const char* settings );
-	virtual const char* SaveSettings();
+  virtual bool SetSettingsFromInterfaces();
+  void UpdateInterfacesFromSettings();
+  virtual void LoadSettings( const char* settings );
+  virtual const char* SaveSettings();
 
 	
-	Channel mInputChannel;
-	U32 mBitRate;
+  Channel mInputChannel;
+  U32 mBitRate;
 
 protected:
-	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mInputChannelInterface;
-	std::auto_ptr< AnalyzerSettingInterfaceInteger >	mBitRateInterface;
+  std::auto_ptr< AnalyzerSettingInterfaceChannel >	mInputChannelInterface;
+  std::auto_ptr< AnalyzerSettingInterfaceInteger >	mBitRateInterface;
 };
 
 #endif //WS2812_ANALYZER_SETTINGS
