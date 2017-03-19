6a19fbadc6fdcf22dfb63b61c5bc0446b57febe5
Ben Laurie <benl@google.com>
2017-03-19 14:42:46 +0000

    Reformat.

--- source/WS2812Analyzer.h
+++ source/WS2812Analyzer.h
@@ -6,35 +6,36 @@
 #include "WS2812SimulationDataGenerator.h"
 
 class WS2812AnalyzerSettings;
-class ANALYZER_EXPORT WS2812Analyzer : public Analyzer
-{
-public:
-	WS2812Analyzer();
-	virtual ~WS2812Analyzer();
-	virtual void WorkerThread();
+class ANALYZER_EXPORT WS2812Analyzer : public Analyzer {
+ public:
+  WS2812Analyzer();
+  virtual ~WS2812Analyzer();
+  virtual void WorkerThread();
 
-	virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
-	virtual U32 GetMinimumSampleRateHz();
+  virtual U32 GenerateSimulationData(U64 newest_sample_requested,
+				     U32 sample_rate,
+				     SimulationChannelDescriptor** simulation_channels);
+  virtual U32 GetMinimumSampleRateHz();
 
-	virtual const char* GetAnalyzerName() const;
-	virtual bool NeedsRerun();
+  virtual const char* GetAnalyzerName() const;
+  virtual bool NeedsRerun();
 
-protected: //vars
-	std::auto_ptr< WS2812AnalyzerSettings > mSettings;
-	std::auto_ptr< WS2812AnalyzerResults > mResults;
-	AnalyzerChannelData* mSerial;
+ protected: //vars
+  std::auto_ptr< WS2812AnalyzerSettings > mSettings;
+  std::auto_ptr< WS2812AnalyzerResults > mResults;
+  AnalyzerChannelData* mSerial;
 
-	WS2812SimulationDataGenerator mSimulationDataGenerator;
-	bool mSimulationInitilized;
+  WS2812SimulationDataGenerator mSimulationDataGenerator;
+  bool mSimulationInitilized;
 
-	//Serial analysis vars:
-	U32 mSampleRateHz;
-	U32 mStartOfStopBitOffset;
-	U32 mEndOfStopBitOffset;
+  // Serial analysis vars:
+  U32 mSampleRateHz;
+  U32 mStartOfStopBitOffset;
+  U32 mEndOfStopBitOffset;
 };
 
 extern "C" ANALYZER_EXPORT const char* __cdecl GetAnalyzerName();
 extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer( );
-extern "C" ANALYZER_EXPORT void __cdecl DestroyAnalyzer( Analyzer* analyzer );
+extern "C" ANALYZER_EXPORT void __cdecl DestroyAnalyzer(Analyzer* analyzer);
 
 #endif //WS2812_ANALYZER_H
