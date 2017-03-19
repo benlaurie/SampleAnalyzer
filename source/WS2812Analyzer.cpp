b73a7362707889655f26c96001eed95451193873
Ben Laurie <benl@google.com>
2017-03-19 14:40:53 +0000

    Reformat.

--- source/WS2812Analyzer.cpp
+++ source/WS2812Analyzer.cpp
@@ -3,109 +3,101 @@
 #include <AnalyzerChannelData.h>
 
 WS2812Analyzer::WS2812Analyzer()
-:	Analyzer(),  
-	mSettings( new WS2812AnalyzerSettings() ),
-	mSimulationInitilized( false )
-{
-	SetAnalyzerSettings( mSettings.get() );
+  : Analyzer(),  
+    mSettings( new WS2812AnalyzerSettings() ),
+    mSimulationInitilized( false ) {
+  SetAnalyzerSettings( mSettings.get() );
 }
 
-WS2812Analyzer::~WS2812Analyzer()
-{
-	KillThread();
+WS2812Analyzer::~WS2812Analyzer() {
+  KillThread();
 }
 
-void WS2812Analyzer::WorkerThread()
-{
-	mResults.reset( new WS2812AnalyzerResults( this, mSettings.get() ) );
-	SetAnalyzerResults( mResults.get() );
-	mResults->AddChannelBubblesWillAppearOn( mSettings->mInputChannel );
+void WS2812Analyzer::WorkerThread() {
+  mResults.reset( new WS2812AnalyzerResults( this, mSettings.get() ) );
+  SetAnalyzerResults( mResults.get() );
+  mResults->AddChannelBubblesWillAppearOn( mSettings->mInputChannel );
 
-	mSampleRateHz = GetSampleRate();
+  mSampleRateHz = GetSampleRate();
 
-	mSerial = GetAnalyzerChannelData( mSettings->mInputChannel );
+  mSerial = GetAnalyzerChannelData( mSettings->mInputChannel );
 
-	if( mSerial->GetBitState() == BIT_LOW )
-		mSerial->AdvanceToNextEdge();
+  if( mSerial->GetBitState() == BIT_LOW )
+    mSerial->AdvanceToNextEdge();
 
-	U32 samples_per_bit = mSampleRateHz / mSettings->mBitRate;
-	U32 samples_to_first_center_of_first_data_bit = U32( 1.5 * double( mSampleRateHz ) / double( mSettings->mBitRate ) );
+  U32 samples_per_bit = mSampleRateHz / mSettings->mBitRate;
+  U32 samples_to_first_center_of_first_data_bit = U32( 1.5 * double( mSampleRateHz ) / double( mSettings->mBitRate ) );
 
-	for( ; ; )
-	{
-		U8 data = 0;
-		U8 mask = 1 << 7;
+  for( ; ; ) {
+    U8 data = 0;
+    U8 mask = 1 << 7;
 		
-		mSerial->AdvanceToNextEdge(); //falling edge -- beginning of the start bit
+    mSerial->AdvanceToNextEdge(); //falling edge -- beginning of the start bit
 
-		U64 starting_sample = mSerial->GetSampleNumber();
+    U64 starting_sample = mSerial->GetSampleNumber();
 
-		mSerial->Advance( samples_to_first_center_of_first_data_bit );
+    mSerial->Advance( samples_to_first_center_of_first_data_bit );
 
-		for( U32 i=0; i<8; i++ )
-		{
-			//let's put a dot exactly where we sample this bit:
-			mResults->AddMarker( mSerial->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mInputChannel );
+    for( U32 i=0; i<8; i++ ) {
+      //let's put a dot exactly where we sample this bit:
+      mResults->AddMarker( mSerial->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mInputChannel );
 
-			if( mSerial->GetBitState() == BIT_HIGH )
-				data |= mask;
+      if( mSerial->GetBitState() == BIT_HIGH )
+	data |= mask;
 
-			mSerial->Advance( samples_per_bit );
+      mSerial->Advance( samples_per_bit );
 
-			mask = mask >> 1;
-		}
+      mask = mask >> 1;
+    }
 
 
-		//we have a byte to save. 
-		Frame frame;
-		frame.mData1 = data;
-		frame.mFlags = 0;
-		frame.mStartingSampleInclusive = starting_sample;
-		frame.mEndingSampleInclusive = mSerial->GetSampleNumber();
+    //we have a byte to save. 
+    Frame frame;
+    frame.mData1 = data;
+    frame.mFlags = 0;
+    frame.mStartingSampleInclusive = starting_sample;
+    frame.mEndingSampleInclusive = mSerial->GetSampleNumber();
 
-		mResults->AddFrame( frame );
-		mResults->CommitResults();
-		ReportProgress( frame.mEndingSampleInclusive );
-	}
+    mResults->AddFrame( frame );
+    mResults->CommitResults();
+    ReportProgress( frame.mEndingSampleInclusive );
+  }
 }
 
-bool WS2812Analyzer::NeedsRerun()
-{
-	return false;
+bool WS2812Analyzer::NeedsRerun() {
+  return false;
 }
 
-U32 WS2812Analyzer::GenerateSimulationData( U64 minimum_sample_index, U32 device_sample_rate, SimulationChannelDescriptor** simulation_channels )
-{
-	if( mSimulationInitilized == false )
-	{
-		mSimulationDataGenerator.Initialize( GetSimulationSampleRate(), mSettings.get() );
-		mSimulationInitilized = true;
-	}
+U32 WS2812Analyzer::GenerateSimulationData(U64 minimum_sample_index,
+					   U32 device_sample_rate,
+					   SimulationChannelDescriptor** simulation_channels ) {
+  if(mSimulationInitilized == false) {
+    mSimulationDataGenerator.Initialize(GetSimulationSampleRate(),
+					mSettings.get());
+    mSimulationInitilized = true;
+  }
 
-	return mSimulationDataGenerator.GenerateSimulationData( minimum_sample_index, device_sample_rate, simulation_channels );
+  return mSimulationDataGenerator.GenerateSimulationData(minimum_sample_index,
+							 device_sample_rate,
+							 simulation_channels);
 }
 
-U32 WS2812Analyzer::GetMinimumSampleRateHz()
-{
-	return mSettings->mBitRate * 4;
+U32 WS2812Analyzer::GetMinimumSampleRateHz() {
+  return mSettings->mBitRate * 4;
 }
 
-const char* WS2812Analyzer::GetAnalyzerName() const
-{
-	return "WS2812";
+const char* WS2812Analyzer::GetAnalyzerName() const {
+  return "WS2812";
 }
 
-const char* GetAnalyzerName()
-{
-	return "WS2812";
+const char* GetAnalyzerName() {
+  return "WS2812";
 }
 
-Analyzer* CreateAnalyzer()
-{
-	return new WS2812Analyzer();
+Analyzer* CreateAnalyzer() {
+  return new WS2812Analyzer();
 }
 
-void DestroyAnalyzer( Analyzer* analyzer )
-{
-	delete analyzer;
+void DestroyAnalyzer( Analyzer* analyzer ) {
+  delete analyzer;
 }
