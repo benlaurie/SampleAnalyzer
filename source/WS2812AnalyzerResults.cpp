471162c3d96dda22ca1ee3348b74c48cadf48cea
Ben Laurie <benl@google.com>
2017-03-19 17:34:08 +0000

    Reformat.

--- source/WS2812AnalyzerResults.cpp
+++ source/WS2812AnalyzerResults.cpp
@@ -5,77 +5,79 @@
 #include <iostream>
 #include <fstream>
 
-WS2812AnalyzerResults::WS2812AnalyzerResults( WS2812Analyzer* analyzer, WS2812AnalyzerSettings* settings )
-:	AnalyzerResults(),
-	mSettings( settings ),
-	mAnalyzer( analyzer )
-{
+WS2812AnalyzerResults::WS2812AnalyzerResults(WS2812Analyzer* analyzer,
+					     WS2812AnalyzerSettings* settings)
+  : AnalyzerResults(),
+    mSettings(settings),
+    mAnalyzer(analyzer) {
 }
 
-WS2812AnalyzerResults::~WS2812AnalyzerResults()
-{
+WS2812AnalyzerResults::~WS2812AnalyzerResults() {
 }
 
-void WS2812AnalyzerResults::GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base )
-{
-	ClearResultStrings();
-	Frame frame = GetFrame( frame_index );
+void WS2812AnalyzerResults::GenerateBubbleText(U64 frame_index,
+					       Channel& channel,
+					       DisplayBase display_base) {
+  ClearResultStrings();
+  Frame frame = GetFrame( frame_index );
 
-	char number_str[128];
-	AnalyzerHelpers::GetNumberString( frame.mData1, display_base, 8, number_str, 128 );
-	AddResultString( number_str );
+  char number_str[128];
+  AnalyzerHelpers::GetNumberString( frame.mData1, display_base, 8, number_str, 128 );
+  AddResultString( number_str );
 }
 
-void WS2812AnalyzerResults::GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id )
-{
-	std::ofstream file_stream( file, std::ios::out );
+void WS2812AnalyzerResults::GenerateExportFile(const char* file,
+					       DisplayBase display_base,
+					       U32 export_type_user_id) {
+  std::ofstream file_stream( file, std::ios::out );
 
-	U64 trigger_sample = mAnalyzer->GetTriggerSample();
-	U32 sample_rate = mAnalyzer->GetSampleRate();
+  U64 trigger_sample = mAnalyzer->GetTriggerSample();
+  U32 sample_rate = mAnalyzer->GetSampleRate();
 
-	file_stream << "Time [s],Value" << std::endl;
+  file_stream << "Time [s],Value" << std::endl;
 
-	U64 num_frames = GetNumFrames();
-	for( U32 i=0; i < num_frames; i++ )
-	{
-		Frame frame = GetFrame( i );
+  U64 num_frames = GetNumFrames();
+  for (U32 i=0; i < num_frames; i++) {
+    Frame frame = GetFrame(i);
 		
-		char time_str[128];
-		AnalyzerHelpers::GetTimeString( frame.mStartingSampleInclusive, trigger_sample, sample_rate, time_str, 128 );
+    char time_str[128];
+    AnalyzerHelpers::GetTimeString(frame.mStartingSampleInclusive,
+				   trigger_sample, sample_rate, time_str, 128);
 
-		char number_str[128];
-		AnalyzerHelpers::GetNumberString( frame.mData1, display_base, 8, number_str, 128 );
+    char number_str[128];
+    AnalyzerHelpers::GetNumberString(frame.mData1, display_base, 8,
+				     number_str, 128);
 
-		file_stream << time_str << "," << number_str << std::endl;
+    file_stream << time_str << "," << number_str << std::endl;
 
-		if( UpdateExportProgressAndCheckForCancel( i, num_frames ) == true )
-		{
-			file_stream.close();
-			return;
-		}
-	}
+    if (UpdateExportProgressAndCheckForCancel( i, num_frames ) == true) {
+      file_stream.close();
+      return;
+    }
+  }
 
-	file_stream.close();
+  file_stream.close();
 }
 
-void WS2812AnalyzerResults::GenerateFrameTabularText( U64 frame_index, DisplayBase display_base )
-{
-	Frame frame = GetFrame( frame_index );
-	ClearResultStrings();
+void WS2812AnalyzerResults::GenerateFrameTabularText(U64 frame_index,
+						   DisplayBase display_base ) {
+  Frame frame = GetFrame(frame_index);
+  ClearResultStrings();
 
-	char number_str[128];
-	AnalyzerHelpers::GetNumberString( frame.mData1, display_base, 8, number_str, 128 );
-	AddResultString( number_str );
+  char number_str[128];
+  AnalyzerHelpers::GetNumberString(frame.mData1, display_base, 8, number_str,
+				   128 );
+  AddResultString(number_str);
 }
 
-void WS2812AnalyzerResults::GeneratePacketTabularText( U64 packet_id, DisplayBase display_base )
-{
-	ClearResultStrings();
-	AddResultString( "not supported" );
+void WS2812AnalyzerResults::GeneratePacketTabularText(U64 packet_id,
+						    DisplayBase display_base) {
+  ClearResultStrings();
+  AddResultString( "not supported" );
 }
 
-void WS2812AnalyzerResults::GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base )
-{
-	ClearResultStrings();
-	AddResultString( "not supported" );
+void WS2812AnalyzerResults::GenerateTransactionTabularText(U64 transaction_id,
+						    DisplayBase display_base) {
+  ClearResultStrings();
+  AddResultString( "not supported" );
 }
